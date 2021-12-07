a5154bef4ac74220d7de1395ba1667eb1ce08ff3
RWelchIII <Avidcompassion@gmail.com>
2021-12-07 14:51:44 -0600

    contactMe page

---
+++ contactMe/contact.js
@@ -0,0 +1,7 @@
+const placeForResults = document.getElementById("contactMe")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+ }
+}
